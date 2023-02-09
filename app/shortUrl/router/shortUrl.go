5e2f8185c6ecaeaf620de07b036bd28caaffc708
Daop <admin@gadmind.com>
2023-02-09 15:56:34 +0800

    Optimization: 🆙Optimization code style and logic

--- app/shortUrl/router/shortUrl.go
+++ app/shortUrl/router/shortUrl.go
@@ -3,58 +3,43 @@ package router
 import (
 	"ShortUrl/app/shortUrl/models"
 	"ShortUrl/common/binary"
+	"ShortUrl/common/constant"
+	"ShortUrl/common/rand"
+	"ShortUrl/common/reids"
+	"ShortUrl/common/resp"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
-	"sync"
 )
 
-var startId = 3844
-var lock sync.RWMutex
-var urlMap = sync.Map{}
-var referMap = sync.Map{}
-
-// GenerateUrl 生成URL
+// GenerateUrl 生成短连接URL
 func GenerateUrl(context *gin.Context) {
-	url := models.UrlInfoPool.Get().(*models.UrlInfo)
-	defer url.Free()
+	urlInfo := models.UrlInfoPool.Get().(*models.UrlInfo)
+	defer urlInfo.Free()
 	body, _ := io.ReadAll(context.Request.Body)
-	_ = json.Unmarshal(body, &url)
-	value, ok := urlMap.Load(url.OriginUrl)
-	if !ok {
-		url.Id = generateId()
-		url.RefCode = binary.ConversionWithBinary(int64(url.Id), 62)
-		urlMap.Store(url.OriginUrl, url.RefCode)
-		referMap.Store(url.RefCode, url.OriginUrl)
-		value = url.RefCode
+	_ = json.Unmarshal(body, &urlInfo)
+
+	originUrl := urlInfo.OriginUrl
+	refCode := reids.Get(constant.SOSPrefix + originUrl)
+	if refCode == "" {
+		randomId := rand.GetRandomId()
+		refCode = binary.ConversionWithBinary(int64(randomId), 62)
+		reids.Set(refCode, constant.SOSPrefix+originUrl, 0)
+		reids.Set(originUrl, constant.SSOPrefix+refCode, 0)
+		// TODO 入库保存
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    http.StatusOK,
-		"success": true,
-		"message": "生成成功",
-		"data":    value,
-	})
+	context.JSON(resp.Success("生成成功", refCode))
 }
 
+// Redirect 短链接重定向
 func Redirect(context *gin.Context) {
 	path, _ := context.Params.Get("path")
-	value, ok := referMap.Load(path)
-	if !ok {
-		context.JSON(200, gin.H{
-			"status":  404,
-			"message": "404 Not Found" + context.Request.Method + " " + context.Request.RequestURI,
-		})
-	} else {
-		n := fmt.Sprintf("%s", value)
-		context.Redirect(http.StatusFound, n)
+	value := reids.Get(constant.SSOPrefix + path)
+	if value == "" {
+		context.JSON(resp.ReNoData(http.StatusNotFound, false, "404 Not Found"+context.Request.Method+" "+context.Request.RequestURI))
 	}
-}
-
-func generateId() int {
-	lock.Lock()
-	defer lock.Unlock()
-	startId += 1
-	return startId
+	n := fmt.Sprintf("%s", value)
+	context.Redirect(http.StatusFound, n)
 }
