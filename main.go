85662a65db5ab1f668741f7048798a7401647fa9
Daop <admin@gadmind.com>
2023-02-24 15:35:14 +0800

    Optimization: Optimization server restart and stop config

--- main.go
+++ main.go
@@ -2,28 +2,52 @@ package main
 
 import (
 	"ShortUrl/app/shortUrl/router"
+	"context"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	server := gin.Default()
-	server.Static("/static", "./static")
-	server.LoadHTMLGlob("templates/**/*")
-	server.GET("/", func(c *gin.Context) {
+	serverDefault := gin.Default()
+	serverDefault.Static("/static", "./static")
+	serverDefault.LoadHTMLGlob("templates/**/*")
+	serverDefault.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index/index.html", gin.H{
 			"title": "index/index",
 		})
 	})
-	server.GET("/:path", router.Redirect)
-	server.POST("/add", router.GenerateUrl)
-	server.POST("/analysis", router.AnalysisUrl)
-	server.DELETE("/delete", router.DeleteUrl)
+	serverDefault.GET("/:path", router.Redirect)
+	serverDefault.POST("/add", router.GenerateUrl)
+	serverDefault.POST("/analysis", router.AnalysisUrl)
+	serverDefault.DELETE("/delete", router.DeleteUrl)
 
-	err := server.Run(":8088")
-	if err != nil {
-		log.Fatalln("ListenAndServer error: ", err)
+	server := &http.Server{
+		Addr:    ":8088",
+		Handler: serverDefault,
 	}
+
+	go func() {
+		// 服务连接
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
