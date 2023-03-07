3a0e94c3c7282791a0a91826d0db63da1751fa71
Daop <admin@gadmind.com>
2023-03-07 13:41:37 +0800

    Feature:➕Add function comment and page view rank

--- app/short_url/db.go
+++ app/short_url/db.go
@@ -5,6 +5,7 @@ import (
 	"log"
 )
 
+// GetUrlInfo 获取链接信息
 func GetUrlInfo() []*UrlInfo {
 	conn := db.MySQLDatabase()
 	sql := `SELECT 
@@ -23,6 +24,7 @@ func GetUrlInfo() []*UrlInfo {
 	return infos
 }
 
+// SaveUrlInfo 保存链接信息
 func SaveUrlInfo(ui *UrlInfo) {
 	conn := db.MySQLDatabase()
 	rs, err := conn.Exec("INSERT INTO url_info(ref_code,origin_url) VALUE(?,?)", ui.RefCode, ui.OriginUrl)
@@ -34,6 +36,7 @@ func SaveUrlInfo(ui *UrlInfo) {
 	log.Printf("插入新数据ID%d", id)
 }
 
+// UpdatePageViewInfo 更新链接访问量
 func UpdatePageViewInfo(ui *UrlInfo) {
 	conn := db.MySQLDatabase()
 	rs, err := conn.Exec("UPDATE url_info SET page_view = ? WHERE ref_code = ?", ui.PageView, ui.RefCode)
@@ -45,6 +48,7 @@ func UpdatePageViewInfo(ui *UrlInfo) {
 	log.Printf("插入新数据ID%d", id)
 }
 
+// DeleteUrlInfo 删除链接信息
 func DeleteUrlInfo(ui *UrlInfo) {
 	conn := db.MySQLDatabase()
 	rs, err := conn.Exec("DELETE FROM url_info WHERE ref_code = ?", ui.RefCode)
@@ -54,4 +58,24 @@ func DeleteUrlInfo(ui *UrlInfo) {
 	}
 	id, err := rs.LastInsertId()
 	log.Printf("插入新数据ID%d", id)
+}
+
+// PageViewRank 链接访问量排行榜
+func PageViewRank() []*UrlInfo {
+	conn := db.MySQLDatabase()
+	sql := `SELECT 
+    			id,
+				ref_code,
+				origin_url,
+				page_view 
+			FROM 
+			    url_info
+			ORDER BY page_view DESC	`
+	infos := make([]*UrlInfo, 0)
+	err := conn.Select(&infos, sql)
+	if err != nil {
+		log.Println("查询出错:", err)
+		return infos
+	}
+	return infos
 }
